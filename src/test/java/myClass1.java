5389b7796becaaa5a45d55b0c3f66772820e500c
daniel <danielgold885@gmail.com>
2019-12-23 11:22:47 +0200

    Changed the other class to remote webdriver

--- src/test/java/myClass1.java
+++ src/test/java/myClass1.java
@@ -2,7 +2,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
--- src/test/java/myClass2.java
+++ src/test/java/myClass2.java
@@ -1,11 +1,18 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class myClass2 {
   @Test
-  public void Test02() throws InterruptedException {
-	  WebDriver driver = new ChromeDriver();
+  public void Test02() throws InterruptedException, MalformedURLException {
+	  DesiredCapabilities chromeDC = DesiredCapabilities.chrome();
+	  
+	  WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), chromeDC);
 	  driver.get("http://atidcollege.co.il/");
 	  driver.manage().window().maximize();
 	  Thread.sleep(4000);
